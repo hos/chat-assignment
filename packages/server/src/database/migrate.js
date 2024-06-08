c0392f4c787076e9cfc83e30b85a916f96bd4261
hos <hovosanoyan@gmail.com>
2024-06-09 01:28:48 +0400

    server: specify schema when creating tables
    this will allow us later delete everything with cascading.

--- packages/server/src/database/migrate.js
+++ packages/server/src/database/migrate.js
@@ -7,23 +7,37 @@ import { pgClient } from "./client.js";
 export async function migrate(drop) {
   if (drop) {
     await pgClient.exec(`--sql
-      drop table if exists tokens;
-      drop table if exists users;
+      drop schema if exists public cascade;
+      create schema public;
     `);
   }
 
   await pgClient.exec(`--sql
-    create table if not exists users (
+    create table if not exists public.users (
       id bigint primary key generated always as identity,
       username text not null unique,
       password_hash text not null,
       created_at timestamp default now()
     );
 
-    create table if not exists tokens (
+    create table if not exists public.tokens (
       id bigint primary key generated always as identity,
       user_id bigint references users(id),
       token text not null,
+      created_at timestamp default now()
+    );
+
+    create table if not exists public.rooms (
+      id bigint primary key generated always as identity,
+      name text not null unique,
+      created_at timestamp default now()
+    );
+
+    create table if not exists public.messages (
+      id bigint primary key generated always as identity,
+      user_id bigint not null references users(id),
+      room_id bigint not null references rooms(id),
+      content text not null,
       created_at timestamp default now()
     );
   `);
