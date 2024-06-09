162052584a79a01d22a3b213a8dd071f7dab3947
hos <hovosanoyan@gmail.com>
2024-06-09 20:36:15 +0400

    server: change password requirements to test easier

--- packages/server/src/utils/zod.js
+++ packages/server/src/utils/zod.js
@@ -8,6 +8,13 @@ export const ZodUsername = z
     message: "Username must be alphanumeric and can contain ._-",
   });
 
-export const ZodPassword = z.string().min(12, {
-  message: "Password must be at least 12 characters long",
-});
+// Here we allow weak passwords as this is not a real project,
+// but in a real project you should enforce strong passwords.
+export const ZodPassword = z
+  .string()
+  .min(2, {
+    message: "Password must be at least 2 characters long",
+  })
+  .max(12, {
+    message: "Password must be at most 12 characters long",
+  });
