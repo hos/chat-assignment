3d3e062f41f9491802626a92652d25ab6ec1e0b8
hos <hovosanoyan@gmail.com>
2024-06-09 05:36:57 +0400

    server: make usernames smaller

--- packages/server/src/utils/zod.js
+++ packages/server/src/utils/zod.js
@@ -3,7 +3,7 @@ import z from "zod";
 export const ZodUsername = z
   .string()
   .min(2)
-  .max(16)
+  .max(12)
   .regex(/^[a-zA-Z0-9_.-]+$/, {
     message: "Username must be alphanumeric and can contain ._-",
   });
