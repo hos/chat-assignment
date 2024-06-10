bce80b168bb75e3b8a69792424703afc0740720e
hos <hovosanoyan@gmail.com>
2024-06-10 13:13:42 +0400

    env,config: automatically load .env from `@chat/config` pkg

--- packages/config/src/index.js
+++ packages/config/src/index.js
@@ -1,3 +1,24 @@
+import { loadEnvFile } from "node:process";
+import { stat } from "node:fs/promises";
+import { resolve } from "node:path";
+
+const env = process.env.NODE_ENV || "development";
+const rootDir = resolve("../../");
+const optionalEnvFile = resolve(rootDir, `${env}.env`);
+
+// First we load more specific env files, configured in `development.env` or `production.env`.
+try {
+  await stat(optionalEnvFile);
+  console.log(`\nLoading environment variables from ${optionalEnvFile}\n`);
+  loadEnvFile(optionalEnvFile);
+} catch (error) {
+  if (error.code !== "ENOENT") {
+    throw error;
+  }
+}
+
+// When more specific file is loaded, we load generic env, which won't override what we already loaded.
+loadEnvFile(resolve(rootDir, ".env"));
 
 export const PORT = process.env.PORT || 3024;
 
@@ -9,5 +30,4 @@ export const DATABASE_URL = process.env.DATABASE_URL;
 
 export const COOKIE_EXPIRY = 60 * 60 * 24 * 7; // 7 days
 
-
-export const REDIS_URL = process.env.REDIS_URL
+export const REDIS_URL = process.env.REDIS_URL;
